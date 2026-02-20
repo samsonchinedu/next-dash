7acbb3f7c473604c3ef9b8ec3a1420951894c22e
samson-quru <samson@qurulabs.com>
2026-02-20 17:16:45 +0100

    Patch 2

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,5 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
+import { lusitana } from '@/app/ui/fonts';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
@@ -6,7 +7,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
             <div className="w-full flex-none md:w-64">
                 <SideNav />
             </div>
-            <div className="grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+            <div className={`${lusitana.className} grow p-6 md:overflow-y-auto md:p-12`}>{children}</div>
         </div>
     );
 }
